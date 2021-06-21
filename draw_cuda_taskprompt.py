931951809ff3b46571b6f7be99b49fd4dccb4c66
Yusheng <ky200120000@gmail.com>
2021-06-21 13:08:46 +0000

    update

--- draw_cuda_taskprompt.py
+++ draw_cuda_taskprompt.py
@@ -95,7 +95,7 @@ def train_AE(input=None, out_features=None):
     # mean-squared error loss
     criterion = nn.MSELoss()
 
-    epochs=30
+    epochs=10
     iterations=100
     model.train()
     for epoch in range(epochs):
@@ -383,7 +383,8 @@ all_label = all_label.to("cpu").numpy()
 
 task_map={0:"sst2",1:"rte",2:"re",3:"MNLI",4:"MRPC",5:"QNLI",6:"QQP",7:"WNLI",8:"STSB",9:"laptop",10:"restaurant"}
 
-color_map={0:"#728FCE",1:"#347235",2:"#3D0C02",3:"#6B8E23",4:"#C04000",5:"QNLI",6:"#CB6D51",7:"#556B2F",8:"STSB",9:"#4863A0",10:"#151B8D"}
+#color_map={0:"#728FCE",1:"#347235",2:"#3D0C02",3:"#6B8E23",4:"#C04000",5:"QNLI",6:"#CB6D51",7:"#556B2F",8:"STSB",9:"#4863A0",10:"#151B8D"}
+color_map={0:"#728FCE",1:"#347235",2:"#3D0C02",3:"#6B8E23",4:"#C04000",5:"#32CD32",6:"#CB6D51",7:"#556B2F",8:"#FFC0CB",9:"#4863A0",10:"#151B8D"}
 
 
 blocked_list = []
@@ -401,7 +402,7 @@ blocked_list = []
 #blocked_list = [1,2,3,5,7,8]
 
 #NLI, Paraphrase
-blocked_list = [0,1,2,5,8,9,10]
+#blocked_list = [0,1,2,5,8,9,10]
 
 #re generate id
 #plot on 3D: https://www.delftstack.com/zh-tw/howto/matplotlib/scatter-plot-legend-in-matplotlib/#%25E5%259C%25A8-matplotlib-3d-%25E6%2595%25A3%25E9%25BB%259E%25E5%259C%2596%25E4%25B8%258A%25E6%2596%25B0%25E5%25A2%259E%25E5%259C%2596%25E4%25BE%258B
