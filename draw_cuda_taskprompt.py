5cc37292e53c4269663230544cf5a623cf97ee44
Yusheng <ky200120000@gmail.com>
2021-06-21 11:22:58 +0000

    update

--- draw_cuda_taskprompt.py
+++ draw_cuda_taskprompt.py
@@ -409,7 +409,7 @@ for task_id, task_name in task_map.items():
 
     print(compressed_prompt_emb[task_id])
 
-    plt.scatter(compressed_prompt_emb[task_id][0], compressed_prompt_emb[task_id][1], color=color_map[task_id], label=task_map[task_id], s=10)
+    plt.scatter(compressed_prompt_emb[task_id][0], compressed_prompt_emb[task_id][1], color=color_map[task_id], label=task_map[task_id], s=50)
 
 #plt.scatter(task_prompt_emb_x_list, task_prompt_emb_y_list, color=color_map[task_id], label=task_prompt_label_list)
 
