090b08297c8cfabfafdfb1991cf1585f39069007
Yusheng <ky200120000@gmail.com>
2021-08-04 20:16:05 +0000

    update

--- draw_cuda_mlmprompt_split.py
+++ draw_cuda_mlmprompt_split.py
@@ -163,8 +163,8 @@ print("===================")
 print("===================")
 
 ##3D or 2D
-#dim=3
-dim=2
+dim=3
+#dim=2
 
 
 #PCA compress
@@ -251,7 +251,7 @@ blocked_list = []
 #plot on 3D: https://www.delftstack.com/zh-tw/howto/matplotlib/scatter-plot-legend-in-matplotlib/#%25E5%259C%25A8-matplotlib-3d-%25E6%2595%25A3%25E9%25BB%259E%25E5%259C%2596%25E4%25B8%258A%25E6%2596%25B0%25E5%25A2%259E%25E5%259C%2596%25E4%25BE%258B
 
 
-if dim ==3 :
+if dim == 3 :
     axes = plt.subplot(111, projection='3d')
     #axes = plt.subplot(222, projection='3d')
 elif dim == 2:
