ef8fb312edcfbc7ff4b3879c680242403692ef68
Yusheng <ky200120000@gmail.com>
2021-09-02 05:32:15 +0000

    update

--- requirement.sh
+++ requirement.sh
@@ -5,5 +5,10 @@ git clone git@github.com:nyu-mll/GLUE-baselines.git
 python3 download_glue_data.py --data_dir glue_data --tasks all
 mv GLUE-baselines data
 
+#Need
 
-
+#BertForMaskedLM
+#RobertaForMaskedLM
+#RobertaLargeForMaskedLM
+#task_prompt_emb
+#data
