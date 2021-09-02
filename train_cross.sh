26812d84f848932222852320cfb3039886111b96
Yusheng <ky200120000@gmail.com>
2021-09-02 14:43:52 +0000

    update

--- train_cross.sh
+++ train_cross.sh
@@ -5,13 +5,13 @@ gpus=1
 #Sentiment
 ############
 
-#model_prompt="bert-base"
+model_prompt="bert-base"
 
 #restaurant
 #CUDA_VISIBLE_DEVICES=$gpus python3 train_projector.py --config config/projectorPromptRoberta.config \
 CUDA_VISIBLE_DEVICES=$gpus python3 train_cross.py --config config/crossPromptRoberta.config \
     --gpu $gpus \
-    #--model_prompt $model_prompt
+    --model_prompt $model_prompt
     #--checkpoint roberta-base \
     #--local_rank \
     #--do_test \
