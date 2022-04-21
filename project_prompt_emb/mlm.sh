7355cd572713d345842c7a34492f9addc7fbc79b
Yusheng <yushengsu.thu@gmail.com>
2022-04-21 16:39:23 +0800

    update

--- project_prompt_emb/mlm.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=7 python3 extract_meta_prompt.py "_mlm_s1" "mlm_s2"
--- project_prompt_emb/task.sh
+++
@@ -1,2 +0,0 @@
-#CUDA_VISIBLE_DEVICES=0 python3 extract_meta_prompt.py "Roberta" "Bert"
-CUDA_VISIBLE_DEVICES=1 python3 extract_meta_prompt.py "Roberta"
