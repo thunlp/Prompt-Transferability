812288a04a031b71f40dc78d48dc3467a1accfeb
Yusheng Su <ky200120000@gmail.com>
2023-07-02 16:15:45 +0800

    update

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,5 @@
 pip install -r requirements.txt
 
-pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install torch
+
+#pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
